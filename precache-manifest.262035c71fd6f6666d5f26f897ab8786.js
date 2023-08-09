155b8e54a164bc32725566c1777e3d8a0c73b1b0
andrewquach-dev <andrewquach-dev@users.noreply.github.com>
2023-08-09 23:36:12 +0000

    deploy: 9648a54a8bcdb801a59c48fd5bd9c79dcef87a45

---
+++ precache-manifest.262035c71fd6f6666d5f26f897ab8786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fa5c97672fee26416ac69f8d0fbb38",
+    "url": "https://andrewquach-dev.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "d80cf4cc55a54d7c31d9",
+    "url": "https://andrewquach-dev.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c74a0bf13f9f330495c0",
+    "url": "https://andrewquach-dev.github.io/task-management-frontend/static/js/2.c6e309eb.chunk.js"
+  },
+  {
+    "revision": "d80cf4cc55a54d7c31d9",
+    "url": "https://andrewquach-dev.github.io/task-management-frontend/static/js/main.e26553b7.chunk.js"
+  },
+  {
+    "revision": "ddf5dcaa075e713bbef1",
+    "url": "https://andrewquach-dev.github.io/task-management-frontend/static/js/runtime~main.e0e50a56.js"
+  }
+]);
